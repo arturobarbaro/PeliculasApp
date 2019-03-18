4e27abd1e00cf9102d1f6b2dd7945a89b270bc2d
Arturo Barba <arturo.barba@iesdonana.org>
2019-03-18 19:44:08 +0100

    Diseño de home

--- src/app/providers/peliculas.service.ts
+++ src/app/providers/peliculas.service.ts
@@ -14,6 +14,18 @@ export class PeliculasService {
       public _http:HttpClient
   ) { }
 
+
+
+  getCartelera(){
+      let desde = new Date();
+      let hasta = new Date(desde.getDate()+7);
+      let desdeStr=`${desde.getFullYear()}-${desde.getMonth()+1}-${desde.getDate()}`;
+      let hastaStr=`${hasta.getFullYear()}-${hasta.getMonth()+1}-${hasta.getDate()}`;
+      
+      const url =`${this.urlMoviedb}/discover/movie?primary_release_date.gte=${desdeStr}&primary_release_date.lte=${hastaStr}&api_key=${this.apikey}&language=es&callback=JSONP_CALLBACK`;
+      return this._http.jsonp(url, '').pipe(map( (res: any) =>  res.results ));
+  }
+
   getPopulares() {
       const url =`${this.urlMoviedb}/discover/movie?sort_by=popularity.desc&api_key=${this.apikey}&language=es&callback=JSONP_CALLBACK`;
       return this._http.jsonp(url, '').pipe(map( (res: any) =>  res.results ));
