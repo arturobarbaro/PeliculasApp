d9727b27bc7a58b180838b685c68d7c309fbe6e8
Arturo Barba <arturo.barba@iesdonana.org>
2019-03-18 21:44:27 +0100

    Mejoras en busquedas

--- src/app/pipes/pelicula-imagen.pipe.ts
+++ src/app/pipes/pelicula-imagen.pipe.ts
@@ -13,6 +13,8 @@ export class PeliculaImagenPipe implements PipeTransform {
       return url + pelicula.backdrop_path;
     } else if( pelicula.poster_path) {
       return url + pelicula.poster_path;
+    } else if(pelicula.backdrop_path){
+      return url + pelicula.backdrop_path;
     } else {
       return "assets/noimage.png";
     }
