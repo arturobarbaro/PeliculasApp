0fb966bb46707e777fe435afb1c128be6a2ca046
Arturo Barba <arturo.barba@iesdonana.org>
2019-03-18 21:36:43 +0100

    Retoques finales

--- src/app/components/pelicula/pelicula.component.ts
+++ src/app/components/pelicula/pelicula.component.ts
@@ -10,13 +10,19 @@ import { ActivatedRoute } from '@angular/router';
 export class PeliculaComponent implements OnInit {
 
   pelicula:any;
+  volverA :string= "";
+  busqueda :string= "";
 
   constructor(public _ps:PeliculasService,
               public router:ActivatedRoute) {
     this.router.params.subscribe(params=>{
         console.log(params);
+        if(params['busqueda']){
+            this.busqueda=params['busqueda'];
+        }
         this._ps.getPelicula(params['id']).subscribe(pelicula=>{
             this.pelicula=pelicula;
+            this.volverA = params['pag'];
         });
     });
   }
